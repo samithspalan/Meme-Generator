d60dde8337403e558dba120578f70178a5cc6183
samithspalan <samithpoojary12345@gmail.com>
2025-03-18 21:04:08 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let btn=document.querySelector(".generate-button");
+let title1=document.querySelector(".meme-title");
+let img=document.querySelector(".meme-image");
+let aut=document.querySelector(".author");
+
 btn.addEventListener("click",()=>{
    fetch("https://meme-api.com/gimme/wholesomememes")
    .then((res)=>res.json())
    .then((data)=>{
       const{title,url,author}=data;
-      console.log(title,url,author)
+      title1.innerText=title
+      img.src=url;
+      title1.style.color="white";
+      aut.innerText=author;
+
    })
 })
